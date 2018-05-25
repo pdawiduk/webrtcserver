4f36fdaa0fb68e7d4ed195f3229a96f7c34580c0
DAWIDUKP <dawidukP@ttpsc.pl>
2018-05-25 11:10:40 +0200

    nmbmbnmbnmnb

---
+++ webrtc/node_modules/wrtc/lib/browser.js
@@ -0,0 +1,5 @@
+'use strict';
+
+exports.RTCIceCandidate = window.RTCIceCandidate;
+exports.RTCPeerConnection = window.RTCPeerConnection;
+exports.RTCSessionDescription = window.RTCSessionDescription;
